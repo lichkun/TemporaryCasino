d2ebabc6a2bfab87c0da62c45821946673073630
nikita_lichkun <nikita.lichkun03@gmail.com>
2024-05-07 18:24:39 +0300

    cm

--- src/app/corousel/corousel.component.ts
+++ src/app/corousel/corousel.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-corousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './corousel.component.html',
-  styleUrl: './corousel.component.scss'
+  styleUrls: ['./corousel.component.scss']
 })
-export class CorouselComponent {
+export class CorouselComponent  {
+  images: string[] = [
+    'assets/img/1852x350_desktop_ua 1.png',
+    'assets/img/1852x350_desktop_ua 1.png',
+    'assets/img/1852x350_desktop_ua 1.png',
+  ];
+
+  currentSlide: number = 0;
+
+  get currentTransform(): string {
+    return `translateX(-${this.currentSlide * 100}%)`;
+  }
+
+  next(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  }
+
+  prev(): void {
+    this.currentSlide = (this.currentSlide - 1) % this.images.length;
+  }
 
 }
