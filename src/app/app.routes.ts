14304ac4b2d67d8747465f8354dbc6ce5b212067
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-09 00:56:09 +0100

    Log In Page Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { RegistrationStep1Component } from './registration-step1/registration-step1.component';
+import { LoginPageComponent } from './Account/login-page/login-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'registration', component: RegistrationStep1Component }
+  { path: 'registration', component: RegistrationStep1Component },
+  { path: 'login', component: LoginPageComponent }
 ];
 
