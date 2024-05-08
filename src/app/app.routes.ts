75eec124afbb42eec337533ebf0e5eb5ce9535c9
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-09 00:36:42 +0100

    Log In Page Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
+=======
+import { MainComponent } from './main/main.component';
+import { RegistrationStep1Component } from './registration-step1/registration-step1.component';
+import { LoginPageComponent } from './Account/login-page/login-page.component';
+
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'registration', component: RegistrationStep1Component },
+  { path: 'login', component: LoginPageComponent }
+];
+>>>>>>> Stashed changes
 
 export const routes: Routes = [];
