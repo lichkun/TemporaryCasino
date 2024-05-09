b49fb3401fa66e4d2cff5ad43499e29f7623f148
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-09 08:29:43 +0100

    User Profile Start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { RegistrationStep1Component } from './registration-step1/registration-step1.component';
 import { LoginPageComponent } from './Account/login-page/login-page.component';
+import { UserProfileComponent } from './Account/user-profile/user-profile.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'registration', component: RegistrationStep1Component },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'user-profile', component: UserProfileComponent }
 ];
 
