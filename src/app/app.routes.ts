3440f290f05049fe2a13808a913951f1aa5ce5e8
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-09 12:12:59 +0100

    Depo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { MainComponent } from './main/main.component';
 import { RegistrationStep1Component } from './registration-step1/registration-step1.component';
 import { LoginPageComponent } from './Account/login-page/login-page.component';
 import { UserProfileComponent } from './Account/user-profile/user-profile.component';
+import { DepositPageComponent } from './Deposit/deposit-page/deposit-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'registration', component: RegistrationStep1Component },
   { path: 'login', component: LoginPageComponent },
-  { path: 'user-profile', component: UserProfileComponent }
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'deposit', component: DepositPageComponent }
 ];
 
