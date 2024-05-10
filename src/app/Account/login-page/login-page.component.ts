7ee36f1155f9df7bb5b3d3eb741a69123a3c80d0
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-10 08:59:49 +0100

    Validation

--- src/app/Account/login-page/login-page.component.ts
+++ src/app/Account/login-page/login-page.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    ReactiveFormsModule,CommonModule
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  form!: FormGroup;
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[a-zA-Z\\d@$!%*?&]{8,}$")
+      ]),
+    });
+    
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Email - ', this.form.get('email')?.value);
+      console.log('Password - ', this.form.get('password')?.value);
+    } else {
+      console.log('Form is bruh');
+    }
+  }
+
+
+  
 }
